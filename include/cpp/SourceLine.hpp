76d01943fc4f8a96ba6727ffad5801b05bbfa63d
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-02-21 17:48:51 +0100

    Basic testing

---
+++ include/cpp/SourceLine.hpp
@@ -0,0 +1,22 @@
+/*!
+ * \file SourceLine.hpp
+ * \brief Exports an object like macro that expands to a C-style string
+ *        of the current line number in the source code.
+**/
+#ifndef INCG_CPPBASE_SOURCELINE_HPP
+#define INCG_CPPBASE_SOURCELINE_HPP
+
+#include "Stringify.hpp" // CPP_STRINGIFY
+
+/*!
+ * \def CPP_SOURCE_LINE
+ * \brief The current line in the current source file.
+ * \note Note that the line number may have been manipulated using #line.
+ *
+ * Macro that expands to a C-String literal of the current line in the current
+ * source file. Equivalent to the stringification of the standard predefined
+ * macro __LINE__
+**/
+#define CPP_SOURCE_LINE CPP_STRINGIFY(__LINE__)
+
+#endif // INCG_CPPBASE_SOURCELINE_HPP
---
+++ include/cpp/Stringify.hpp
@@ -0,0 +1,26 @@
+/*!
+ * \file Stringify.hpp
+ * \brief Exports a macro that can be used to turn any sequence of tokens
+ *        into a C-string literal at preprocessing time.
+**/
+#ifndef INCG_CPPBASE_STRINGIFY_HPP
+#define INCG_CPPBASE_STRINGIFY_HPP
+
+/*!
+ * \def CPP_DETAIL_STRINGIFY(tokens)
+ * \brief Implementation macro for CPP_STRINGIFY
+ * \warning Do not use.
+ * \see CPP_STRINGIFY
+**/
+#define CPP_DETAIL_STRINGIFY(tokens) #tokens
+
+/*!
+ * \def CPP_STRINGIFY(tokens)
+ * \brief Turns a contiguous sequence of tokens passed in into a
+ *        C-String literal.
+ * \example CPP_STRINGIFY(helloworld) results in:
+ *          "helloworld"
+**/
+#define CPP_STRINGIFY(tokens) CPP_DETAIL_STRINGIFY(tokens)
+
+#endif // INCG_CPPBASE_STRINGIFY_HPP
