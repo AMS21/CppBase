281daa2aeb61006cc5155f3a45ebaa94b9f6726d
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-05-16 13:11:08 +0200

    Fix readability magic number warning

--- test/src/Constexpr.test.cpp
+++ test/src/Constexpr.test.cpp
@@ -2,29 +2,32 @@
 
 #include <cpp/Constexpr.hpp>
 
+// Define a magic number to return
+#define MAGIC_NUMBER 42
+
 CPP_CONSTEXPR int constexpr_func_1()
 {
-    return 42;
+    return MAGIC_NUMBER;
 }
 
 CPP_CONSTEXPR_AND_CONST int constexpr_func_2()
 {
-    return 42;
+    return MAGIC_NUMBER;
 }
 
 void constexpr_func_3()
 {
-    CPP_CONSTEXPR_OR(const) int Val = 42;
+    CPP_CONSTEXPR_OR(const) int Val = MAGIC_NUMBER;
 }
 
 CPP_CONSTEXPR_OR_CONST int constexpr_func_4()
 {
-    return 42;
+    return MAGIC_NUMBER;
 }
 
 CPP_CONSTEXPR_OR_INLINE int constexpr_func_5()
 {
-    return 42;
+    return MAGIC_NUMBER;
 }
 
 CPP_EXTENDED_CONSTEXPR void constexpr_func_6()
@@ -32,7 +35,7 @@ CPP_EXTENDED_CONSTEXPR void constexpr_func_6()
 
 void constexpr_func_7()
 {
-    CPP_EXTENDED_CONSTEXPR_OR(const) int Val2 = 42;
+    CPP_EXTENDED_CONSTEXPR_OR(const) int Val2 = MAGIC_NUMBER;
 }
 
 CPP_EXTENDED_CONSTEXPR_OR_CONST void constexpr_func_8()
@@ -43,7 +46,7 @@ CPP_EXTENDED_CONSTEXPR_OR_INLINE void constexpr_func_9()
 
 void constexpr_func_10()
 {
-    auto lam = []() CPP_CONSTEXPR_LAMBDA -> int { return 42; };
+    auto lam = []() CPP_CONSTEXPR_LAMBDA -> int { return MAGIC_NUMBER; };
 
     lam();
 }
