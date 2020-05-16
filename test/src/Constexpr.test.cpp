946d979eb8ad2caca9b50d4cbc2e5ecbb311c949
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-05-16 16:57:46 +0200

    Improve constexpr tests

--- test/src/Constexpr.test.cpp
+++ test/src/Constexpr.test.cpp
@@ -44,9 +44,23 @@ CPP_EXTENDED_CONSTEXPR_OR_CONST void constexpr_func_8()
 CPP_EXTENDED_CONSTEXPR_OR_INLINE void constexpr_func_9()
 {}
 
-void constexpr_func_10()
+int constexpr_func_10()
 {
     auto lam = []() CPP_CONSTEXPR_LAMBDA -> int { return MAGIC_NUMBER; };
 
-    lam();
+    return lam();
+}
+
+TEST_CASE("Constexpr")
+{
+    CHECK_EQ(constexpr_func_1(), MAGIC_NUMBER);
+    CHECK_EQ(constexpr_func_2(), MAGIC_NUMBER);
+    constexpr_func_3();
+    CHECK_EQ(constexpr_func_4(), MAGIC_NUMBER);
+    CHECK_EQ(constexpr_func_5(), MAGIC_NUMBER);
+    constexpr_func_6();
+    constexpr_func_7();
+    constexpr_func_8();
+    constexpr_func_9();
+    CHECK_EQ(constexpr_func_10(), MAGIC_NUMBER);
 }
