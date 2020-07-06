e71a8c67bb1ec6355761298509f98adcac9e5b3f
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-07-06 18:42:09 +0200

    Fix likely macro for if cases

--- include/cpp/Likely.hpp
+++ include/cpp/Likely.hpp
@@ -5,14 +5,12 @@
 #include "FunctionLikeMacro.hpp"
 
 #if CPP_COMPILER_IS(CPP_COMPILER_GCC)
+#    define CPP_LIKELY(condition) __builtin_expect(!!(condition), 1)
+#    define CPP_UNLIKELY(condition) __builtin_expect(!!(condition), 0)
 #    if CPP_COMPILER_VERSION_IS_ATLEAST(9, 0, 0)
-#        define CPP_LIKELY(condition) [[likely]] condition
-#        define CPP_UNLIKELY(condition) [[unlikely]] condition
 #        define CPP_LIKELY_CASE [[likely]]
 #        define CPP_UNLIKELY_CASE [[unlikely]]
 #    else
-#        define CPP_LIKELY(condition) __builtin_expect(!!(condition), 1)
-#        define CPP_UNLIKELY(condition) __builtin_expect(!!(condition), 0)
 #        define CPP_LIKELY_CASE   /* Nothing */
 #        define CPP_UNLIKELY_CASE /* Nothing */
 #    endif
@@ -27,8 +25,8 @@
 #    define CPP_LIKELY_CASE   /* Nothing */
 #    define CPP_UNLIKELY_CASE /* Nothing */
 #elif CPP_COMPILER_IS_ATLEAST(CPP_COMPILER_MSVC, 19, 26, 0)
-#    define CPP_LIKELY(condition) [[likely]] condition
-#    define CPP_UNLIKELY(condition) [[unlikely]] condition
+#    define CPP_LIKELY(condition) condition
+#    define CPP_UNLIKELY(condition) condition
 #    define CPP_LIKELY_CASE [[likely]]
 #    define CPP_UNLIKELY_CASE [[unlikely]]
 #else
