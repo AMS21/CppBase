48df91678b98b0ce3da370112fd02d671fd70f25
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-07-06 18:25:44 +0200

    Update likely to support standard syntax for GCC and MSVC

--- include/cpp/Likely.hpp
+++ include/cpp/Likely.hpp
@@ -5,8 +5,13 @@
 #include "FunctionLikeMacro.hpp"
 
 #if CPP_COMPILER_IS(CPP_COMPILER_GCC)
-#    define CPP_LIKELY(condition) __builtin_expect(!!(condition), 1)
-#    define CPP_UNLIKELY(condition) __builtin_expect(!!(condition), 0)
+#    if CPP_COMPILER_VERSION_IS_ATLEAST(9, 0, 0)
+#        define CPP_LIKELY(condition) [[likely]] condition
+#        define CPP_UNLIKELY(condition) [[unlikey]] condition
+#    else
+#        define CPP_LIKELY(condition) __builtin_expect(!!(condition), 1)
+#        define CPP_UNLIKELY(condition) __builtin_expect(!!(condition), 0)
+#    endif
 #elif CPP_COMPILER_IS(CPP_COMPILER_CLANG)
 #    if __has_builtin(__builtin_expect)
 #        define CPP_LIKELY(condition) __builtin_expect(!!(condition), 1)
@@ -15,6 +20,9 @@
 #        define CPP_LIKELY(condition) condition
 #        define CPP_UNLIKELY(condition) condition
 #    endif
+#elif CPP_COMPILER_IS_ATLEAST(CPP_COMPILER_MSVC, 19, 26, 0)
+#    define CPP_LIKELY(condition) [[likely]] condition
+#    define CPP_UNLIKELY(condition) [[unlikely]] condition
 #else
 #    define CPP_LIKELY(condition) condition
 #    define CPP_UNLIKELY(condition) condition
